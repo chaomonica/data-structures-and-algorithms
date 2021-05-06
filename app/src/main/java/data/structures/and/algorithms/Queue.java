b74e968b78ad7691da8e10186de39fc9bb108ca7
chaomonica <monica.y.chao@gmail.com>
2021-05-06 11:16:06 -0500

    implemented and completed tests for queue

--- app/src/main/java/data/structures/and/algorithms/Queue.java
+++ app/src/main/java/data/structures/and/algorithms/Queue.java
@@ -1,4 +1,57 @@
 package data.structures.and.algorithms;
 
 public class Queue {
+    private static class QNode {
+        private int data;
+        private QNode next;
+        private QNode(int data) {
+            this.data = data;
+        }
+    }
+
+    private QNode head;
+    private QNode tail;
+
+    public boolean isEmpty() {
+        if (this.head == null && this.tail == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void add(int data) {
+        QNode newNode = new QNode(data);
+        if (this.head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+    }
+
+    public int peek() {
+        return this.head.data;
+    }
+
+    public int remove() {
+        int removed = this.head.data;
+        if (this.head.next != null) {
+            this.head = this.head.next;
+        } else {
+            this.head = null;
+            this.tail = null;
+        }
+        return removed;
+    }
+
+    public boolean isTailNull() {
+        if (this.tail == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
+
---
+++ app/src/test/java/data/structures/and/algorithms/QueueTest.java
@@ -0,0 +1,75 @@
+package data.structures.and.algorithms;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class QueueTest {
+
+    private Queue testQueue;
+    @BeforeEach
+    void setup() {
+        testQueue = new Queue();
+    }
+
+    @Test
+    void newQueueShouldExist() {
+        Queue actual = new Queue();
+        assertNotNull(actual);
+    }
+
+    @Test
+    void newQueueShouldBeEmpty() {
+        boolean expected = true;
+        boolean actual = testQueue.isEmpty();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void queueShouldNotBeEmptyAfterNodeAdded() {
+        boolean expected = false;
+        testQueue.add(3);
+        boolean actual = testQueue.isEmpty();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void peekShouldReturnFirstItemInQueue() {
+        int expected = 3;
+        testQueue.add(3);
+        int actual = testQueue.peek();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void removeShouldRemoveFirstItemInQueueAndReturnData() {
+        testQueue.add(3);
+        testQueue.add(4);
+        int expected = 3;
+        int actual = testQueue.remove();
+        assertEquals(expected, actual);
+        assertEquals(4, testQueue.peek());
+    }
+
+    @Test
+    void removeShouldRemoveFirstItemInQueueOfLengthOne() {
+        testQueue.add(3);
+        testQueue.remove();
+        boolean expected = true;
+        boolean actual = testQueue.isEmpty();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void tailShouldBeEmptyIfOnlyItemInQueueIsRemoved() {
+        testQueue.add(3);
+        testQueue.remove();
+        boolean expected = true;
+        boolean actual = testQueue.isTailNull();
+        assertEquals(expected, actual);
+    }
+
+}
+
